938c978f62194c1cdb62e902ac23d4205305f7b0
Naeem1144 <aknaeem246@gmail.com>
2025-05-07 12:37:28 -0400

    Fix ESLint errors: Remove unused variable and escape quotes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,6 @@
 import React from 'react';
 
 export function Footer() {
-  const currentYear = new Date().getFullYear();
-
   return (
     <footer className="py-6 border-t border-white/10">
       <div className="container mx-auto px-4 text-center">
